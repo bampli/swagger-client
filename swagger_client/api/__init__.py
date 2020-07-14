debc56e1c975f1b0212d405a224def2bfaa96c81
Jose Motta <josemotta@bampli.com>
2020-07-14 20:01:55 -0300

    cleaning

--- swagger_client/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.company_api import CompanyApi
-from swagger_client.api.cyclo_api import CycloApi
-from swagger_client.api.facility_api import FacilityApi
