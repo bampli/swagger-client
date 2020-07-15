9aaedd74ab28d42f37b59e05f4c1cd3b44512bcb
Jose Motta <josemotta@bampli.com>
2020-07-15 13:39:34 -0300

    fix tests with swagger_client.

--- test/test_task.py
+++ test/test_task.py
@@ -15,7 +15,7 @@ from __future__ import absolute_import
 import unittest
 
 import swagger_client
-from models.task import Task  # noqa: E501
+from swagger_client.models.task import Task  # noqa: E501
 from swagger_client.rest import ApiException
 
 
